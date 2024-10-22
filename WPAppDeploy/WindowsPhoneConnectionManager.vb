2efa533a2f233a33be4606a0c200b579c3437660
PC-DOS <suicunexp@outlook.com>
2024-10-22 16:04:36 +0800

    Update WindowsPhoneConnectionManager.vb

--- WPAppDeploy/WindowsPhoneConnectionManager.vb
+++ WPAppDeploy/WindowsPhoneConnectionManager.vb
@@ -5,24 +5,76 @@ Imports Microsoft.SmartDevice.Connectivity
 Imports Microsoft.SmartDevice.Connectivity.Interface
 Imports Microsoft.SmartDevice.MultiTargeting.Connectivity
 Public Class WindowsPhoneConnectionManager
+    ''' <summary>
+    ''' Structure of a connected phone device.
+    ''' </summary>
+    ''' <remarks></remarks>
     Public Structure PhoneDevice
+        ''' <summary>
+        ''' Windows Phone device operating interface.
+        ''' </summary>
+        ''' <remarks></remarks>
         Public ConnectedDevice As IDevice
+        ''' <summary>
+        ''' Raw device model.
+        ''' </summary>
+        ''' <remarks></remarks>
         Public RawDevice As ConnectableDevice
     End Structure
+    ''' <summary>
+    ''' Internal manager of phone connectivity.
+    ''' </summary>
+    ''' <remarks></remarks>
     Private _PhoneConnectionManager As New MultiTargetingConnectivity(System.Globalization.CultureInfo.CurrentCulture.LCID)
+    ''' <summary>
+    ''' Flag of the presence of a connected phone.
+    ''' </summary>
+    ''' <remarks></remarks>
     Private _IsPhoneConnected As Boolean
+    ''' <summary>
+    ''' Instance of a connected phone.
+    ''' </summary>
+    ''' <remarks></remarks>
     Private _ConnectedPhone As New PhoneDevice
 
+    ''' <summary>
+    ''' Thread for asynchronous phone connecting.
+    ''' </summary>
+    ''' <remarks></remarks>
     Private Class PhoneConnectingThread
+        ''' <summary>
+        ''' Instance of the phone pending connection.
+        ''' </summary>
+        ''' <remarks></remarks>
         Private _PhoneToConnect As ConnectableDevice
+        ''' <summary>
+        ''' Instance of connedted phone.
+        ''' </summary>
+        ''' <remarks></remarks>
         Private _ConnectedPhone As IDevice
+        ''' <summary>
+        ''' Create a new child thread.
+        ''' </summary>
+        ''' <param name="PhoneToConnect">Instance of the phone to be connected.</param>
+        ''' <remarks></remarks>
         Public Sub New(PhoneToConnect As ConnectableDevice)
             _PhoneToConnect = PhoneToConnect
         End Sub
+        ''' <summary>
+        ''' Connect to target.
+        ''' </summary>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
         Public Function ConnectToPhone() As IDevice
             _ConnectedPhone = _PhoneToConnect.Connect()
             Return _ConnectedPhone
         End Function
+        ''' <summary>
+        ''' Instance of the phone to be connected.
+        ''' </summary>
+        ''' <value>Instance of the phone to be connected.</value>
+        ''' <returns>Instance of the phone to be connected.</returns>
+        ''' <remarks></remarks>
         Public Property PhoneToConnect As ConnectableDevice
             Get
                 Return _PhoneToConnect
@@ -31,6 +83,12 @@ Public Class WindowsPhoneConnectionManager
                 _PhoneToConnect = value
             End Set
         End Property
+        ''' <summary>
+        ''' Instance of connedted phone.
+        ''' </summary>
+        ''' <value>Instance of connedted phone.</value>
+        ''' <returns>Instance of connedted phone.</returns>
+        ''' <remarks></remarks>
         Public ReadOnly Property ConnectedPhone As IDevice
             Get
                 Return _ConnectedPhone
@@ -38,9 +96,18 @@ Public Class WindowsPhoneConnectionManager
         End Property
     End Class
 
+    ''' <summary>
+    ''' Create a new phone connection manager instance.
+    ''' </summary>
+    ''' <remarks></remarks>
     Public Sub New()
         _IsPhoneConnected = False
     End Sub
+    ''' <summary>
+    ''' Enumerate available devices.
+    ''' </summary>
+    ''' <returns>List of available devices.</returns>
+    ''' <remarks></remarks>
     Public Function EnumerateDevices() As List(Of ConnectableDevice)
         Dim PhoneList As New List(Of ConnectableDevice)
         For Each Phone As ConnectableDevice In _PhoneConnectionManager.GetConnectableDevices()
@@ -49,6 +116,10 @@ Public Class WindowsPhoneConnectionManager
         Return PhoneList
     End Function
 
+    ''' <summary>
+    ''' Disconnect from connected phone.
+    ''' </summary>
+    ''' <remarks></remarks>
     Public Sub DiconnectFromPhone()
         Try
             _ConnectedPhone.ConnectedDevice.Disconnect()
@@ -58,6 +129,11 @@ Public Class WindowsPhoneConnectionManager
         _IsPhoneConnected = False
     End Sub
 
+    ''' <summary>
+    ''' Connect to phone synchronously.
+    ''' </summary>
+    ''' <param name="PhoneToConnect">Instance of the phone to be connected.</param>
+    ''' <remarks></remarks>
     Public Sub ConnectToPhone(PhoneToConnect As ConnectableDevice)
         Try
             Dim ConnectedDevice As New PhoneDevice
@@ -73,6 +149,12 @@ Public Class WindowsPhoneConnectionManager
         End Try
     End Sub
 
+    ''' <summary>
+    ''' Connect to phone asynchronously.
+    ''' </summary>
+    ''' <param name="PhoneToConnect">Instance of the phone to be connected.</param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
     Public Async Function ConnectToPhoneAsync(PhoneToConnect As ConnectableDevice) As Task
         Try
             Dim PhoneConnector As New PhoneConnectingThread(PhoneToConnect)
@@ -88,6 +170,12 @@ Public Class WindowsPhoneConnectionManager
         End Try
     End Function
 
+    ''' <summary>
+    ''' Install an application package to connected phone.
+    ''' </summary>
+    ''' <param name="AppPackagePath">Path to the application package.</param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
     Public Function InstallAppPackage(AppPackagePath As String) As IRemoteApplication
         Dim AppGuid As System.Guid
         Dim AppGenre As String
@@ -110,12 +198,25 @@ Public Class WindowsPhoneConnectionManager
                                                              AppIconPath, _
                                                              AppPackagePath)
     End Function
+
+    ''' <summary>
+    ''' Flag of the presence of a connected phone.
+    ''' </summary>
+    ''' <value></value>
+    ''' <returns>Flag of the presence of a connected phone.</returns>
+    ''' <remarks></remarks>
     Public ReadOnly Property IsPhoneConnected() As Boolean
         Get
             Return _IsPhoneConnected
         End Get
     End Property
 
+    ''' <summary>
+    ''' Instance of a connected phone.
+    ''' </summary>
+    ''' <value></value>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
     Public ReadOnly Property ConnectedPhone() As PhoneDevice
         Get
             If _IsPhoneConnected Then
